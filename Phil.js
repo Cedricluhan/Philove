6268d54da8d9a722bfc122aa7df7761bfe675a11
Cedricluhan <91421922+Cedricluhan@users.noreply.github.com>
2021-10-03 14:05:18 -0400

    Add files via upload

---
+++ Phil.js
@@ -0,0 +1,12 @@
+var result = confirm("Welcome to Love");
+if (result == true) {
+    alert("thank for visiting ");
+} else {
+    alert(" thank us for staying with us");
+}
+var user = prompt("Please enter password");
+if (user == cedric) {
+    alert('welcome');
+} else {
+    alert('xori');
+}
